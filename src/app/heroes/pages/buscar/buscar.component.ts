50e1479297f96d9e341954fd287e9509750deb94
Arley Rivas G <arivas@innside.co>
2022-07-17 18:35:45 -0500

    Autocomplete

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Heroe } from '../../interfaces/heroes.interfaces';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
@@ -8,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+  heroes: Heroe [] = [];
+  heroeSeleccionado: Heroe | undefined;
+  noHayHeroes: boolean = false;
 
-  ngOnInit(): void {
+  constructor( private heroesService: HeroesService) { }
+
+  ngOnInit(): void {}
+
+  buscando(){
+      this.heroesService.getSugerencias(this.termino.trim())  
+        .subscribe(heroes => {
+          this.heroes = heroes;
+          if(heroes.length>0){
+            this.noHayHeroes = false;
+          }else{
+            this.noHayHeroes = true;
+          }
+        });
   }
+
+  opcionSeleccionada( event: MatAutocompleteSelectedEvent ){
+    if(!event.option.value){ //si no no optiene valor (string vacio) 
+      this.heroeSeleccionado = undefined;
+      return;
+    } 
+      const heroe: Heroe = event.option.value;
+      this.termino = heroe.superhero;
+      
+      this.heroesService.getHeroePorId( heroe.id!)
+      .subscribe( heroe =>  this.heroeSeleccionado = heroe);
+   }
 
 }
