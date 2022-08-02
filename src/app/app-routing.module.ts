804384c7b18cc2693b1faa01429d3c88daf45996
Arley Rivas G <arivas@innside.co>
2022-08-02 01:02:46 -0500

    Guard CandLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/guards/auth.guard';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
   {
     path:'auth',
-    loadChildren: () => import('./auth/auth.module').then( module => module.AuthModule) // importamos el modulo ya que en el se encuentra todo importado
-                                                     //con el import se cargua en memoria entonces (.then de promesa resuelta) regresa el modulo AuthModule    
+    loadChildren: () => import('./auth/auth.module').then( module => module.AuthModule) // importamos el modulo ya que en el se encuentra todo importado,
+                                                       //con el import se cargua en memoria entonces (.then de promesa resuelta) regresa el modulo AuthModule    
   },
   {
     path:'heroes',
-    loadChildren: () => import('./heroes/heroes.module').then(module => module.HeroesModule)
+    loadChildren: () => import('./heroes/heroes.module').then(module => module.HeroesModule),
+    canLoad: [AuthGuard]  
   },
   {
     path: '404',
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -25,4 +25,6 @@ export class AuthService {
             tap(usuarioAuth => this._usuarioLogeado = usuarioAuth)
           );
   }
+
+  
 }
